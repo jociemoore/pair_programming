4ab250e09b66bb1e1c13f9db9bdc87ffa10bffe0
Jocie Moore <hello@jociemoore.com>
2017-07-07 06:42:30 -0400

    Create BowlingTest.java

---
+++ BowlingTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class BowlingTest {
+
+	private Bowling bowling;
+
+	@Before 
+	public void setup() {
+		bowling = new Bowling();
+	}
+
+	@Test
+	public void shouldOutputAScoreGivenOneTurn() throws Exception {
+		bowling.bowl(5);
+		int score = bowling.score();
+		assertEquals(5, score);
+	}
+
+	@Test
+	public void shouldOutputScoreForEmptyGame() throws Exception {
+		bowling.bowl(0);
+		bowling.bowl(0);
+
+		bowling.bowl(0);
+		bowling.bowl(0);
+
+		bowling.bowl(0);
+		bowling.bowl(0);
+
+		bowling.bowl(0);
+		bowling.bowl(0);
+
+		int score = bowling.score();
+		assertEquals(0, score);
+	}
+
+}
